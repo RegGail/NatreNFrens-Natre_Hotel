8441d2e9fdf8ecb414881753114f8c4289cc07fd
ellysinday <163708267+ellysinday@users.noreply.github.com>
2024-06-16 12:57:52 +0800

    hindi ko na alam anong file pero ito yung nasa C

---
+++ NatreHote;/PAGES/BOOKING/Booking3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NatreHote_.PAGES.BOOKING
+{
+    public partial class BOOKINGpg3 : Form
+    {
+        public BOOKINGpg3()
+        {
+            InitializeComponent();
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+            panel3.BackColor = Color.FromArgb(100, 0, 0, 0);
+        }
+
+        private void placeHolderTextBox1_TextChanged(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void label18_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
